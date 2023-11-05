da985e3bb6fd34ef61b23eeccadc3188b9b37ad9
Anil517 <53336408+Anil517@users.noreply.github.com>
2023-11-05 18:05:26 +0530

    Update Main.tf

--- Terraform/Main.tf
+++ Terraform/Main.tf
@@ -1,1 +1,27 @@
+# code need to be added here.
 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.78.0"
+    }
+  }
+  backend "azurerm"{
+  }
+}
+
+provider "azurerm" {
+    features {}
+  # Configuration options
+
+  subscription_id   = var.subscription_id
+  tenant_id         = var.tenant_id
+  client_id         = var.client_id
+  client_secret     = var.client_secret
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "AnilRG"
+  location = "West Europe"
+}
